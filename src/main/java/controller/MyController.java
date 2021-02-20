7966920e1a9ea35303aa6e6fa34344483c50b96e
ajongs <parkwonjong1030@gmail.com>
2021-02-20 19:04:12 +0900

    ReqeustMethod(POST, PUT, DELETE) 추가

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -3,10 +3,7 @@ package controller;
 import mapper.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,40 +14,28 @@ public class MyController {
 
     @ResponseBody
     @RequestMapping(value="/select")
-    public List<String> select(){
+    public List<Member> select(){
         return mapper.select();
     }
 
     @ResponseBody
-    @RequestMapping(value="/insert")
-    public List<String> insert(){
-        mapper.insert();
-        return mapper.select();
-    }
-    @ResponseBody
-    @RequestMapping(value="/insert2")
-    public List<String> insert2(){
-        mapper.insert2();
-        return mapper.select();
-    }
-    @ResponseBody
-    @RequestMapping(value="/insert3/{name}", method = RequestMethod.GET)
-    public List<String> insert3(@PathVariable String name){
-        mapper.insert3(name);
-        return mapper.select();
+    @RequestMapping(value="/insert", produces = "application/json; charset=utf8", method = RequestMethod.POST)
+    public String insert(@RequestBody Member member){
+        mapper.insert(member);
+        return member.getName() + " 데이터 추가완료";
     }
 
     @ResponseBody
-    @RequestMapping(value="/update")
-    public List<String> update(){
-        mapper.update();
-        return mapper.select();
+    @RequestMapping(value="/update", produces = "application/json; charset=utf8", method = RequestMethod.PUT)
+    public String update(@RequestBody Member member){
+        mapper.update(member);
+        return member.getName() + " update";
     }
 
     @ResponseBody
-    @RequestMapping(value="/delete")
-    public List<String> delete(){
-        mapper.delete();
-        return mapper.select();
+    @RequestMapping(value="/delete", produces = "application/json; charset=utf8", method = RequestMethod.DELETE)
+    public String delete(@RequestBody Member member){
+        mapper.delete(member);
+        return "delete " + member.getName();
     }
 }
--- src/main/java/mapper/Mapper.java
+++ src/main/java/mapper/Mapper.java
@@ -1,5 +1,6 @@
 package mapper;
 
+import controller.Member;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
@@ -11,19 +12,16 @@ import java.util.List;
 @Repository
 public interface Mapper {
     @Select("select * from member")
-    List<String> select();
+    List<Member> select();
 
-    @Insert("insert into member(name) values('Lee')")
-    void insert();
-    @Insert("insert into member(name) values('Park')")
-    void insert2();
 
-    @Insert("insert into member(name) values(#{name})")
-    void insert3(String name);
+    @Insert("insert into member(name, age) values(#{name}, #{age})")
+    void insert(Member member);
 
-    @Update("update member set name='wonjong' where name = 'park'")
-    void update();
+    @Update("update member set age = #{age} where name = #{name} ")
+    void update(Member member);
 
-    @Delete("Delete from member where name = 'park'")
-    void delete();
+
+    @Delete("Delete from member where name = #{name}")
+    void delete(Member member);
 }
